5b4b7a1f1eec4fe9e26cf92c0b75c91f0e113641
tonkotsurobot <een625@gmail.com>
2020-06-07 14:55:51 +1000

    Moved backend to remote nfs drive

--- vcenter.tf
+++ vcenter.tf
@@ -30,11 +30,3 @@ data "vsphere_virtual_machine" "template" {
   name          = var.template_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-
-data "terraform_remote_state" "foo" {
-  backend = "local"
-
-  config = {
-    path = "/mnt/nfs-drive-1/tf-centos-base/terraform.tfstate"
-  }
-}
--- vm.tf
+++ vm.tf
@@ -1,3 +1,9 @@
+terraform {
+  backend "local" {
+    path = "/mnt/nfs-drive-1/tf-centos-base/terraform.tfstate"
+  }
+}
+
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
   datastore_id     = data.vsphere_datastore.datastore.id
