01a8314373e594cec07bba3cf361e0a338e146cc
Edward Mario <emario@tyro.com>
2020-07-01 11:37:08 +1000

    test docker-compose

---
+++ scripts/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -xeu
+
+echo "see what's on /workdir"
+ls -la /workdir
+
+echo "see what's on /mnt/tf-centos-base"
+ls -la /mnt/tf-centos-base
+#echo "+++ :terraform: Terraform Init"
+#terraform init -backend=false
+#
+#echo "+++ :terraform: Terraform Validate"
+#for file in vars/*.tfvars
+#do
+#    terraform validate -var-file=${file}
+#done
