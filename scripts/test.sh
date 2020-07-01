534f1fe56532783f6086783b966f36513ef45eda
Edward Mario <emario@tyro.com>
2020-07-01 18:00:52 +1000

    try docker compose

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ echo "see what's on /workdir"
 ls -la
 
 echo "see what's on /mnt/tf-centos-base"
-ls -la /mnt/tf-centos-base
+ls -la /mnt/nfs-drive-1/tf-centos-base
 #echo "+++ :terraform: Terraform Init"
 #terraform init -backend=false
 #
