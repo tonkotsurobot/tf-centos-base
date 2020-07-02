d88b56a83e713730231e66d0c616e14f2fa45336
Edward Mario <emario@tyro.com>
2020-07-02 11:08:43 +1000

    move variables to .tfvars file

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -8,5 +8,5 @@ wget http://192.168.1.30:8000/scripts/terraform_vcenter_cred.sh -O /tmp/terrafor
 chmod u+x /tmp/terraform_vcenter_cred.sh
 . /tmp/terraform_vcenter_cred.sh
 terraform init
-terraform destroy -input=false -var template_name=centos-base -var vm_name=centos-vm -var ip_address=192.168.1.60  -var vsphere_datastore=esx-nfs-1 -auto-approve
+terraform destroy -auto-approve
 rm -rf .terraform
