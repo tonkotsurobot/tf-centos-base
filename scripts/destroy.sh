e1f5a7291c4ec2a8fa9657c6888ae4e7e76d2695
tonkotsurobot <een625@gmail.com>
2020-07-26 11:04:32 +1000

    Update base

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -8,5 +8,5 @@ wget http://192.168.1.30:8000/scripts/terraform_vcenter_cred.sh -O /tmp/terrafor
 chmod u+x /tmp/terraform_vcenter_cred.sh
 . /tmp/terraform_vcenter_cred.sh
 terraform init -backend-config=backend_config.hcl
-terraform destroy -auto-approve
+terraform destroy -input=false -var template_name=$TEMPLATE_NAME -var vm_name=$VM_NAME -var ip_address=$IP_ADDRESS  -var vsphere_datastore=esx-nfs-1 -auto-approve
 rm -rf .terraform
